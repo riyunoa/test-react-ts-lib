5527ec4e72f9703b8acf72d408400eb93edc287e
gillian.ng <gillian.ng@elmolearning.com.au>
2018-12-07 16:53:25 +1100

    Trying to autogenerate ts definitions

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-interface ButtonProps {
+export interface ButtonProps {
   label: string;
 }
 
--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export interface ButtonProps {
-  label: string;
-}
-
-export class Button extends React.Component<ButtonProps> {}
